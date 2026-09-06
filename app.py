62b77829d8569476caf1528971423decc4444ddb
mmussawar140-design <mmussawar140@gmail.com>
2026-09-06 15:40:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,15 +20,15 @@ st.set_page_config(page_title="Superior College Okara Portal", layout="wide", pa
 FIREBASE_URL = "https://superior-college-okara-9efbc-default-rtdb.firebaseio.com/" 
 
 if not firebase_admin._apps:
-try:
-    # راز (Secret) سے چابی پڑھنا
-    key_dict = json.loads(st.secrets["firebase_secret"])
-    cred = credentials.Certificate(key_dict)
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': FIREBASE_URL
-    })
-except Exception as e:
-    st.error(f"Firebase Connection Error: {e}")
+    try:
+        # راز (Secret) سے چابی پڑھنا
+        key_dict = json.loads(st.secrets["firebase_secret"])
+        cred = credentials.Certificate(key_dict)
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': FIREBASE_URL
+        })
+    except Exception as e:
+        st.error(f"Firebase Connection Error: {e}")
 
 def load_data(node_name, default_val=[]):
     try:
